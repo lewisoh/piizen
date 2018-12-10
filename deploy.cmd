ca965fad1a73b6f5cde4e331dc86809126792ae1
Lewis <lewis.oh@pg.com>
2018-12-10 16:42:04 +0000

    '7'

--- deploy.cmd
+++ deploy.cmd
@@ -123,7 +123,7 @@ echo Upgrade pip package.
 env\scripts\python -m pip install pip --upgrade
 IF !ERRORLEVEL! NEQ 0 goto error
 echo Pip install requirements
-env\scripts\pip install murmurhash-1.0.1-cp36-cp36m-win_amd64.whl
+env\scripts\pip install murmurhash3
 env\scripts\pip install -r requirements.txt
 
 IF !ERRORLEVEL! NEQ 0 goto error
