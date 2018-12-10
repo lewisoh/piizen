f0c2c5423e7661342eae424a4cb23c26652aa52e
Lewis <lewis.oh@pg.com>
2018-12-10 16:44:05 +0000

    '7'

--- deploy.cmd
+++ deploy.cmd
@@ -123,7 +123,6 @@ echo Upgrade pip package.
 env\scripts\python -m pip install pip --upgrade
 IF !ERRORLEVEL! NEQ 0 goto error
 echo Pip install requirements
-env\scripts\pip install murmurhash3
 env\scripts\pip install -r requirements.txt
 
 IF !ERRORLEVEL! NEQ 0 goto error
