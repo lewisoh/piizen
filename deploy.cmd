52344b436d04a6651ace73c0f1e63f85cdb4e802
Lewis <lewis.oh@pg.com>
2018-12-12 14:59:28 +0000

    '12/12'

--- deploy.cmd
+++ deploy.cmd
@@ -122,8 +122,8 @@ IF NOT EXIST "%DEPLOYMENT_TARGET%\env\azure.env.%PYTHON_RUNTIME%.txt" (
 echo Configure Wheel
 D:\home\python364x64\python.exe -m pip install wheel
 IF !ERRORLEVEL! NEQ 0 goto error
-echo Pip install requirements. 
-D:\home\python364x64\python.exe -m pip install –upgrade -r D:\home\site\wwwroot\requirements.txt
+cho Pip install requirements. 
+D:\home\python364x64\python.exe -m pip install -–upgrade -r D:\home\site\wwwroot\requirements.txt
 
 IF !ERRORLEVEL! NEQ 0 goto error
 
