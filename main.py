e0a2c14853fe8184638ac1851771c538677b6b41
Lewis <lewis.oh@pg.com>
2018-12-13 13:43:34 +0000

    '12/12'

--- main.py
+++ main.py
@@ -3,45 +3,42 @@ import spacy
 import re
 import json
 
-    
-# nlp = spacy.load('en_core_web_sm')
+	
+nlp = spacy.load('en_core_web_sm')
 app = Flask(__name__)
 
-print('hellllo')
+
         
-@app.route("/") #methods=['POST'])
+@app.route("/", methods=['POST'])
 def parse_text():
-    return 'Hello, World!'
 
-    # input = request.get_json()
-    # print(input)
-    # sentence = json.dumps(input)
+    input = request.get_json()
+    print(input)
+    sentence = json.dumps(input)
     
     
     
 
-    # doc = nlp(sentence)    
-    # sentence = doc.text
+    doc = nlp(sentence)    
+    sentence = doc.text
 
-    # for ent in doc.ents:
-    #     if ent.label_ == 'PERSON':
-    #         sentence = re.sub(ent.text, '**PII-NAME**', sentence)
-    #     if ent.label_ == 'ORG':
-    #         sentence = re.sub(ent.text, '**PII-NAME**', sentence)
-    #     if ent.label_ == 'NUM':
-    #         sentence = re.sub(ent.text, '**PII-NUM**', sentence) 
-    #     if ent.label_ == 'DATE':
-    #         sentence = re.sub(ent.text, '**PII-NUM**', sentence)
-    #     if ent.label_ == 'CARDINAL':
-    #         sentence = re.sub(ent.text, '**PII-NUM**', sentence)
-    #     print("test")
-    #     print("sentence")
+    for ent in doc.ents:
+        if ent.label_ == 'PERSON':
+            sentence = re.sub(ent.text, '**PII-NAME**', sentence)
+        if ent.label_ == 'ORG':
+            sentence = re.sub(ent.text, '**PII-NAME**', sentence)
+        if ent.label_ == 'NUM':
+            sentence = re.sub(ent.text, '**PII-NUM**', sentence) 
+        if ent.label_ == 'DATE':
+            sentence = re.sub(ent.text, '**PII-NUM**', sentence)
+        if ent.label_ == 'CARDINAL':
+            sentence = re.sub(ent.text, '**PII-NUM**', sentence)
+        print("test")
+        print("sentence")
 
     
 
 
     #sentence = " ".join(sentence)
 
-    
-if __name__ == "__main__":
-    app.run()
+    return sentence
