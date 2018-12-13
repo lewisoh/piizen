746a8e6bcaf958557aefcd108925ba0d7ee18c6c
Lewis <lewis.oh@pg.com>
2018-12-13 13:24:15 +0000

    '12/12'

--- deploy.cmd
+++ deploy.cmd
@@ -2,7 +2,7 @@
 
 :: ----------------------
 :: KUDU Deployment Script
-:: Version: 1.0.17
+:: Version: 1.0.8
 :: ----------------------
 
 :: Prerequisites
@@ -17,8 +17,6 @@ IF %ERRORLEVEL% NEQ 0 (
 
 :: Setup
 :: -----
-
-echo HERE WE GO INSTALLING WITH MY OWN PYTHON VERSION !!
 
 setlocal enabledelayedexpansion
 
@@ -56,10 +54,28 @@ goto Deployment
 
 :SelectPythonVersion
 
-  SET PYTHON_RUNTIME=python-3.6
-  SET PYTHON_VER=3.6
-  SET PYTHON_EXE=%SYSTEMDRIVE%\home\python364x64\python.exe
-  SET PYTHON_ENV_MODULE=venv
+IF DEFINED KUDU_SELECT_PYTHON_VERSION_CMD (
+  call %KUDU_SELECT_PYTHON_VERSION_CMD% "%DEPLOYMENT_SOURCE%" "%DEPLOYMENT_TARGET%" "%DEPLOYMENT_TEMP%"
+  IF !ERRORLEVEL! NEQ 0 goto error
+
+  SET /P PYTHON_RUNTIME=<"%DEPLOYMENT_TEMP%\__PYTHON_RUNTIME.tmp"
+  IF !ERRORLEVEL! NEQ 0 goto error
+
+  SET /P PYTHON_VER=<"%DEPLOYMENT_TEMP%\__PYTHON_VER.tmp"
+  IF !ERRORLEVEL! NEQ 0 goto error
+
+  SET /P PYTHON_EXE=<"%DEPLOYMENT_TEMP%\__PYTHON_EXE.tmp"
+  IF !ERRORLEVEL! NEQ 0 goto error
+
+  SET /P PYTHON_ENV_MODULE=<"%DEPLOYMENT_TEMP%\__PYTHON_ENV_MODULE.tmp"
+  IF !ERRORLEVEL! NEQ 0 goto error
+) ELSE (
+  SET PYTHON_RUNTIME=python-2.7
+  SET PYTHON_VER=2.7
+  SET PYTHON_EXE=%SYSTEMDRIVE%\python27\python.exe
+  SET PYTHON_ENV_MODULE=virtualenv
+)
+
 goto :EOF
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -78,48 +94,13 @@ IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
 IF NOT EXIST "%DEPLOYMENT_TARGET%\requirements.txt" goto postPython
 IF EXIST "%DEPLOYMENT_TARGET%\.skipPythonDeployment" goto postPython
 
-echo Detected requirements.txt.  You can skip Python specific steps with a .skipPythonDeployment file.
+echo Detected requirements.txt. RUNNING CUSTOM DEPLOYMENT
 
-:: 2. Select Python version
-call :SelectPythonVersion
-
-pushd "%DEPLOYMENT_TARGET%"
-
-echo Python install Wheels
-%PYTHON_EXE% -m pip install --upgrade pip
-%PYTHON_EXE% -m pip install wheel
-
-echo Python  Wheels install wheelhouse repository
-%PYTHON_EXE% -m pip wheel -r %DEPLOYMENT_TARGET%\requirements.txt -w wheelhouse
-
-echo Python install requirements.
-%PYTHON_EXE% -m pip install --upgrade -r %DEPLOYMENT_TARGET%\requirements.txt
-
+:: 2. Install packages
+echo Pip install requirements.
+D:\home\Python27\python.exe -m pip install --upgrade -r requirements.txt
 IF !ERRORLEVEL! NEQ 0 goto error
 
-REM Add additional package installation here
-REM -- Example --
-REM env\scripts\easy_install pytz
-REM IF !ERRORLEVEL! NEQ 0 goto error
-
-:: 5. Copy web.config
-IF EXIST "%DEPLOYMENT_SOURCE%\web.%PYTHON_VER%.config" (
-  echo Overwriting web.config with web.%PYTHON_VER%.config
-  copy /y "%DEPLOYMENT_SOURCE%\web.%PYTHON_VER%.config" "%DEPLOYMENT_TARGET%\web.config"
-)
-
-:: 6. Django collectstatic
-IF EXIST "%DEPLOYMENT_TARGET%\manage.py" (
-  IF EXIST "%DEPLOYMENT_TARGET%\env\lib\site-packages\django" (
-    IF NOT EXIST "%DEPLOYMENT_TARGET%\.skipDjango" (
-      echo Collecting Django static files. You can skip Django specific steps with a .skipDjango file.
-      IF NOT EXIST "%DEPLOYMENT_TARGET%\static" (
-        MKDIR "%DEPLOYMENT_TARGET%\static"
-      )
-      env\scripts\python manage.py collectstatic --noinput --clear
-    )
-  )
-)
 
 popd
 
